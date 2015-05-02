6ad04bfc0fefa6e6c2be4788170908d78d45d577
R. Aidan Campbell <aidan.campbell@gmail.com>
2015-05-02 13:19:37 -0400

    now with slightly prettier strings

--- main.rb
+++ main.rb
@@ -19,26 +19,31 @@ later = "Several Hours:\nhigh: "+ high_temp[1] + ' low: '+low[1] + ' and general
 
 #we want an umbrella if there's rain/showers now or soon
 umbrella = data[0..1].collect {|x| x['conditions'].downcase}
-                      .collect{|x| x.include?('rain') or x.include?('shower')}.reduce(:|)#dat data flow
+                      .collect{|x| x.include?('rain') or x.include?('shower')}.reduce(:|) ? 'yes' : 'no' #dat data flow
 
 #we never want cargo shorts
-cargo_shorts = false
+cargo_shorts = 'no'
 
 #we want shorts if the average temp across now and 'future' is above 60, and it's not raining.
 temps = high_temp.map{|x|x.to_i}.reduce(:+)+low.map{|x|x.to_i}.reduce(:+)
-shorts = ((temps / 4) > 60) and not umbrella
-
+shorts = (((temps / 4) > 60) and umbrella.include?('no'))
 #we want pants if the average temp across now and 'future' is below 50
 pants = ((temps / 4) < 50)
 
+bottom = ''
+bottom = 'shorts' if shorts
+bottom = 'pants' if pants
+
 #we want jacket if the average temp across now and 'future' is below 40
-jacket = ((temps / 4) < 40)
+jacket = ((temps / 4) < 40) ? 'yes' : 'no'
 
-
-text = "\ncargo shorts? #{cargo_shorts}\nshorts? #{shorts}\npants? #{pants}\njacket? #{jacket}\numbrella? #{umbrella}\n\n\n#{immediate}\n\n#{later}"
-
+text = "\ncargo shorts? #{cargo_shorts}"
+text <<"\nbottom: #{bottom}" if pants or shorts
+text <<"\njacket? #{jacket}"
+text <<"\numbrella? #{umbrella}"
+text <<"\n\n#{immediate}"
+text <<"\n\n#{later}"
 puts text
-
 
 # # Make the SendGrid client
 # client = SendGrid::Client.new(api_user: SENDGRID_USERNAME, api_key: SENDGRID_PASSWORD)
