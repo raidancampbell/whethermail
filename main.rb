12366425fdfd0016aba1c68f133d317b211ee53c
R. Aidan Campbell <aidan.campbell@gmail.com>
2015-05-01 22:39:45 -0400

    Basic functioning implementation

--- main.rb
+++ main.rb
@@ -1,22 +1,31 @@
-gem 'sendgrid-ruby'
 
 require_relative 'auth.private'
+require 'wunderground'
+require 'sendgrid-ruby'
 
+
+# Make the Wunderground client
 w_api = Wunderground.new(WUNDERGROUND_KEY)
+# Make the call
+data = w_api.forecast_for('44106')['forecast']['simpleforecast']['forecastday'][0..1]
+immediate = "Now:\nhigh: "+ data[0]['high']['fahrenheit'] + ' low: '+data[0]['low']['fahrenheit'] + ' and generally: '+data[0]['conditions'].downcase
+later = "Several Hours:\nhigh: "+ data[1]['high']['fahrenheit'] + ' low: '+data[1]['low']['fahrenheit'] + ' and generally: '+data[1]['conditions'].downcase
+puts immediate
+puts later
 
 
 
-# Make the client
+# Make the SendGrid client
 client = SendGrid::Client.new(api_user: SENDGRID_USERNAME, api_key: SENDGRID_PASSWORD)
 
-# Make the email
+# Make the SendGrid email
 mail = SendGrid::Mail.new do |m|
-  m.to = 'aidan.campbell@gmail.com'
-  m.from = 'taco@cat.limo'
-  m.subject = 'Hello world!'
-  m.text = 'I heard you like pineapple.'
+  m.to = 'USER@EMAIL.com' #your email here
+  m.from = 'weather@tacocat.land'
+  m.subject = 'Today\'s Weather Digest'
+  m.text = immediate+"\n\n"+later
 end
 
 # Send the email and report the status.
-puts client.send(mail)
+puts client.send(mail)['message']
 # {"message":"success"}
